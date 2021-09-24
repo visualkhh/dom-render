9241b04394c84261f06ae8724c1ad23fb992a396
khh <visualkhh@gmail.com>
2021-09-24 11:05:52 +0900

    Dom control and reorder using web component version up

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 import {ConstructorType} from './types/Types';
-import { RawSet } from 'RawSet';
+import { RawSet } from './RawSet';
 
 export type TargetElement = { name: string, callBack: (target: Element, obj: any, rawSet: RawSet) => DocumentFragment, complete?: (target: Element, obj: any, rawSet: RawSet) => void };
 export type TargetAttr = { name: string, callBack: (target: Element, attrValue: string, obj: any, rawSet: RawSet) => DocumentFragment, complete?: (target: Element, attrValue: string, obj: any, rawSet: RawSet) => void };
@@ -7,7 +7,7 @@ export type TargetAttr = { name: string, callBack: (target: Element, attrValue: 
 export interface Config {
     targetElements?: TargetElement[];
     targetAttrs?: TargetAttr[];
-    onElementInit?:(name: string, obj: any,rawSet: RawSet) => void;
+    onElementInit?:(name: string, obj: any, rawSet: RawSet) => void;
     onAttrInit?:(name: string, attrValue: string, obj: any, rawSet: RawSet) => void;
     proxyExcludeTyps?: ConstructorType<any>[];
     applyEvents?: { attrName: string, callBack: (elements: Element, attrValue: string, obj: any) => void }[];
