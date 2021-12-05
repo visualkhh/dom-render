c14e7883ac8169b8f1acdf3080c9ec351e722091
visualkhh <visualkhh@gmail.com>
2021-12-05 12:37:20 +0900

    validation

--- src/validators/Validator.ts
+++ src/validators/Validator.ts
@@ -200,6 +200,14 @@ export abstract class Validator<T = any, E = Element> {
         return Object.entries(this).filter(([k, v]) => (v instanceof Validator));
     }
 
+    public childValidValidator(): [string, Validator][] {
+        return this.childValidator().filter(it => it[1].valid());
+    }
+
+    public childInValidValidator(): [string, Validator][] {
+        return this.childValidator().filter(it => it[1].inValid());
+    }
+
     public get length() {
         return (this.value as any)?.length ?? 0;
     }
